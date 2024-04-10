870d67ae5cfe593dd2c43d37b48a30f4118d595b
dempseyt <dempsey.thompson@journeyone.com.au>
2024-04-10 12:41:26 +0800

    First Commit

---
+++ src/components/ShoeGrid/ShoeGrid.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+
+import SHOES from '../../data';
+import ShoeCard from '../ShoeCard';
+
+const ShoeGrid = () => {
+  return (
+    <Wrapper>
+      {SHOES.map((shoe) => (
+        <ShoeWrapper>
+          <ShoeCard key={shoe.slug} {...shoe} />
+        </ShoeWrapper>
+      ))}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 32px;
+`;
+
+const ShoeWrapper = styled.div`
+  min-width: 275px;
+  max-width: 400px;
+  flex: 1;
+`;
+
+export default ShoeGrid;
